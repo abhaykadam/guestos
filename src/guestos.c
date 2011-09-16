4b42129dba3fb0ea4223037b440627b7a84a0414
Abhay Kadam <abhaykadam88@gmail.com>
2011-09-16 13:29:28 +0530

    changed virtual disk's name to Sim_disk

--- src/guestos.c
+++ src/guestos.c
@@ -173,7 +173,7 @@ void set_defaults(void) {
 	tracks=atoi(param_value);
 	get_param("NUM_SECTORS",param_value);
 	sectors=atoi(param_value);
-	sprintf(command,"dd if=/dev/zero of=disk.img bs=%dx%dx%db count=1",heads,tracks,sectors);
+	sprintf(command,"dd if=/dev/zero of=Sim_disk bs=%dx%dx%db count=1",heads,tracks,sectors);
 	system(command);
 }
 
